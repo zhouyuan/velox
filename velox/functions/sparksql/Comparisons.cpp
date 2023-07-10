0163078bcd8906e73e5489c41f6d085f11233a3e
mayan <yan.ma@intel.com>
2023-07-10 09:57:39 +0000

    Add comparison Spark functions (5569)

--- velox/functions/sparksql/Comparisons.cpp
+++ velox/functions/sparksql/Comparisons.cpp
@@ -13,15 +13,90 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include "velox/functions/sparksql/LeastGreatest.h"
 
-#include <utility>
-
-#include "velox/expression/DecodedArgs.h"
-#include "velox/expression/VectorFunction.h"
+#include "velox/expression/EvalCtx.h"
+#include "velox/expression/Expr.h"
 #include "velox/functions/sparksql/Comparisons.h"
+#include "velox/type/Type.h"
 
 namespace facebook::velox::functions::sparksql {
 namespace {
+
+template <typename Cmp, TypeKind kind>
+class ComparisonFunction final : public exec::VectorFunction {
+  using T = typename TypeTraits<kind>::NativeType;
+
+  bool supportsFlatNoNullsFastPath() const override {
+    return true;
+  }
+
+  void apply(
+      const SelectivityVector& rows,
+      std::vector<VectorPtr>& args,
+      const TypePtr& outputType,
+      exec::EvalCtx& context,
+      VectorPtr& result) const override {
+    context.ensureWritable(rows, BOOLEAN(), result);
+    auto* flatResult = result->asFlatVector<bool>();
+    const Cmp cmp;
+
+    exec::DecodedArgs decodedArgs(rows, args, context);
+    auto decodedArg0 = decodedArgs.at(0);
+    auto decodedArg1 = decodedArgs.at(1);
+    if (decodedArg0->isIdentityMapping() && decodedArg1->isConstantMapping()) {
+      rows.applyToSelected([&](auto i) {
+        flatResult->set(
+            i, cmp(decodedArg0->valueAt<T>(i), decodedArg1->valueAt<T>(0)));
+      });
+    } else if (
+        decodedArg0->isConstantMapping() && decodedArg1->isIdentityMapping()) {
+      rows.applyToSelected([&](auto i) {
+        flatResult->set(
+            i, cmp(decodedArg0->valueAt<T>(0), decodedArg1->valueAt<T>(i)));
+      });
+    } else {
+      rows.applyToSelected([&](auto i) {
+        flatResult->set(
+            i, cmp(decodedArg0->valueAt<T>(i), decodedArg1->valueAt<T>(i)));
+      });
+    }
+  }
+};
+
+template <template <typename> class Cmp>
+std::shared_ptr<exec::VectorFunction> makeImpl(
+    const std::string& functionName,
+    const std::vector<exec::VectorFunctionArg>& args) {
+  VELOX_CHECK_EQ(args.size(), 2);
+  for (size_t i = 1; i < args.size(); i++) {
+    VELOX_CHECK(*args[i].type == *args[0].type);
+  }
+  switch (args[0].type->kind()) {
+#define SCALAR_CASE(kind)                            \
+  case TypeKind::kind:                               \
+    return std::make_shared<ComparisonFunction<      \
+        Cmp<TypeTraits<TypeKind::kind>::NativeType>, \
+        TypeKind::kind>>();
+    SCALAR_CASE(BOOLEAN)
+    SCALAR_CASE(TINYINT)
+    SCALAR_CASE(SMALLINT)
+    SCALAR_CASE(INTEGER)
+    SCALAR_CASE(BIGINT)
+    SCALAR_CASE(HUGEINT)
+    SCALAR_CASE(REAL)
+    SCALAR_CASE(DOUBLE)
+    SCALAR_CASE(VARCHAR)
+    SCALAR_CASE(VARBINARY)
+    SCALAR_CASE(TIMESTAMP)
+#undef SCALAR_CASE
+    default:
+      VELOX_NYI(
+          "{} does not support arguments of type {}",
+          functionName,
+          args[0].type->kind());
+  }
+}
 
 template <TypeKind kind>
 void applyTyped(
@@ -121,7 +196,43 @@ class EqualtoNullSafe final : public exec::VectorFunction {
         flatResult);
   }
 };
+
 } // namespace
+
+std::shared_ptr<exec::VectorFunction> makeEqualTo(
+    const std::string& functionName,
+    const std::vector<exec::VectorFunctionArg>& args,
+    const core::QueryConfig& /*config*/) {
+  return makeImpl<Equal>(functionName, args);
+}
+
+std::shared_ptr<exec::VectorFunction> makeLessThan(
+    const std::string& functionName,
+    const std::vector<exec::VectorFunctionArg>& args,
+    const core::QueryConfig& /*config*/) {
+  return makeImpl<Less>(functionName, args);
+}
+
+std::shared_ptr<exec::VectorFunction> makeGreaterThan(
+    const std::string& functionName,
+    const std::vector<exec::VectorFunctionArg>& args,
+    const core::QueryConfig& /*config*/) {
+  return makeImpl<Greater>(functionName, args);
+}
+
+std::shared_ptr<exec::VectorFunction> makeLessThanOrEqual(
+    const std::string& functionName,
+    const std::vector<exec::VectorFunctionArg>& args,
+    const core::QueryConfig& /*config*/) {
+  return makeImpl<LessOrEqual>(functionName, args);
+}
+
+std::shared_ptr<exec::VectorFunction> makeGreaterThanOrEqual(
+    const std::string& functionName,
+    const std::vector<exec::VectorFunctionArg>& args,
+    const core::QueryConfig& /*config*/) {
+  return makeImpl<GreaterOrEqual>(functionName, args);
+}
 
 std::shared_ptr<exec::VectorFunction> makeEqualNullSafe(
     const std::string& name,
@@ -131,5 +242,4 @@ std::shared_ptr<exec::VectorFunction> makeEqualNullSafe(
       std::make_shared<EqualtoNullSafe>();
   return kEqualtoNullSafeFunction;
 }
-
 } // namespace facebook::velox::functions::sparksql
