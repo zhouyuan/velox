b843947d1257a6d92003270eef3509ccb7ab455d
Yuan Zhou <yuan.zhou@intel.com>
2023-11-20 13:44:08 +0800

    update setup adapters.sh
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- scripts/setup-adapters.sh
+++ scripts/setup-adapters.sh
@@ -113,11 +113,11 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
    # information and is available everywhere
    LINUX_DISTRIBUTION=$(. /etc/os-release && echo ${ID})
    if [[ "$LINUX_DISTRIBUTION" == "ubuntu" ]]; then
-      apt install -y --no-install-recommends libxml2-dev libgsasl7-dev uuid-dev
+      sudo apt install -y --no-install-recommends libxml2-dev libgsasl7-dev uuid-dev
       # Dependencies of GCS, probably a workaround until the docker image is rebuilt
-      apt install -y --no-install-recommends libc-ares-dev libcurl4-openssl-dev
+      sudo apt install -y --no-install-recommends libc-ares-dev libcurl4-openssl-dev
       # Dependencies of Azure Storage Blob cpp
-      apt install -y openssl
+      sudo apt install -y openssl
    else # Assume Fedora/CentOS
       yum -y install libxml2-devel libgsasl-devel libuuid-devel
       # Dependencies of GCS, probably a workaround until the docker image is rebuilt
