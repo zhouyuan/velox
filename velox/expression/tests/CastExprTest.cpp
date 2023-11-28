0fbd3460cd1169018bed593655b5305f849d0f3f
Yuan Zhou <yuan.zhou@intel.com>
2023-11-28 19:53:00 +0800

    enable cast decimal tests
    Signed-off-by: Yuan Zhou <yuan.zhou@intel.com>

--- velox/expression/tests/CastExprTest.cpp
+++ velox/expression/tests/CastExprTest.cpp
@@ -1620,7 +1620,6 @@ TEST_F(CastExprTest, decimalToVarchar) {
       "c0", longFlatForZero, makeNullableFlatVector<StringView>({"0"}));
 }
 
-/*
 TEST_F(CastExprTest, decimalToDecimal) {
   // short to short, scale up.
   auto shortFlat =
@@ -1728,15 +1727,15 @@ TEST_F(CastExprTest, decimalToDecimal) {
           makeNullableFlatVector<int128_t>(
               {DecimalUtil::kLongDecimalMax}, DECIMAL(38, 0)),
           makeNullableFlatVector<int128_t>({0}, DECIMAL(38, 1))),
-      "Cannot cast DECIMAL '99999999999999999999999999999999999999' to
-DECIMAL(38, 1)"); VELOX_ASSERT_THROW( testComplexCast( "c0",
+      "Cannot cast DECIMAL '99999999999999999999999999999999999999' to DECIMAL(38, 1)");
+  VELOX_ASSERT_THROW(
+      testComplexCast(
+          "c0",
           makeNullableFlatVector<int128_t>(
               {DecimalUtil::kLongDecimalMin}, DECIMAL(38, 0)),
           makeNullableFlatVector<int128_t>({0}, DECIMAL(38, 1))),
-      "Cannot cast DECIMAL '-99999999999999999999999999999999999999' to
-DECIMAL(38, 1)");
+      "Cannot cast DECIMAL '-99999999999999999999999999999999999999' to DECIMAL(38, 1)");
 }
-*/
 
 TEST_F(CastExprTest, integerToDecimal) {
   testIntToDecimalCasts<int8_t>();
@@ -1770,7 +1769,6 @@ TEST_F(CastExprTest, boolToDecimal) {
 }
 
 // The result is obtained by select cast('31.4e-2' as decimal(12, 2)).
-/*
 TEST_F(CastExprTest, varcharToDecimal) {
   auto input = makeFlatVector<StringView>(
       {"9999999999.99",
@@ -1963,8 +1961,7 @@ TEST_F(CastExprTest, varcharToDecimal) {
           "c0",
           makeConstant<StringView>("111111111111111111.23", 1),
           makeConstant<int128_t>(8, 1, DECIMAL(38, 38))),
-      "Cannot cast VARCHAR '111111111111111111.23' to DECIMAL(38, 38). Value too
-large.")
+      "Cannot cast VARCHAR '111111111111111111.23' to DECIMAL(38, 38). Value too large.")
 
   VELOX_ASSERT_THROW(
       testComplexCast(
@@ -1994,7 +1991,6 @@ large.")
           makeConstant<int128_t>(12, 1, DECIMAL(38, 0))),
       "Cannot cast VARCHAR ' 1.23 ' to DECIMAL(38, 0). Value is not a number")
 }
-*/
 
 TEST_F(CastExprTest, castInTry) {
   // Test try(cast(array(varchar) as array(bigint))) whose input vector is
